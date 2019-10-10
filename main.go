5aa91027d39cbaffc3457b08d3c9ed8b90aa240a
João Victor da Costa Gonçalves <joaog@kyros.com.br>
2019-10-10 16:40:13 -0300

    composition

--- main.go
+++ main.go
@@ -2,8 +2,45 @@ package main
 
 import (
 	"fmt"
+	"encoding/json"
 )
 
+type person struct{
+	F_name 	string	`json:"first"`
+	L_name 	string	`json:"last"`
+}
+
+type student struct{
+	person
+	Studying bool	`json:"studying"`
+}
+
+func (p person) create(){
+	fmt.Println("Creating person")
+}
+
 func main(){
-	fmt.Println("first commit")
+
+	personOne := person {
+		F_name: "Person",
+		L_name: "One",
+	}
+	personOne.create()
+
+	personOneJSON, _ := json.Marshal(personOne)
+	fmt.Println(string(personOneJSON))
+
+
+	studentOne := student{
+		person: person{
+			F_name: "Student",
+			L_name:"One",
+		},
+		Studying: true,
+	}
+	studentOne.create()
+
+	studentOneJSON, _ := json.Marshal(studentOne)
+	fmt.Println(string(studentOneJSON))
+	
 }
